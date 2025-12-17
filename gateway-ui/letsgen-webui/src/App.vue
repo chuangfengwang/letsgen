ce42922a18cc45e7f9a527ef3fd469010bda2ebc
chuangfeng.wang <chuangfeng.wang@qunar.com>
2025-12-17 23:52:56 +0800

    前端初始化

---
+++ gateway-ui/letsgen-webui/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <router-link to="/">首页</router-link> |
+    <router-link to="/about">关于</router-link>
+  </div>
+  <router-view></router-view>
+</template>
+
+<style scoped></style>
---
+++ gateway-ui/letsgen-webui/src/router/index.ts
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../pages/home.vue'
+import About from '../pages/about.vue'
+
+const routes = [
+  {
+    path: '/', name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About', component: About
+  }
+]
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: routes,
+})
+
+export default router
